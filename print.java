dbacd2d7f061db0622ee3f903e2e7d5ef68119f4
Raj-Kirit-Soni <rajkirit123doc@gmail.com>
2026-05-17 15:06:16 +0530

    this is the first change after a long time

---
+++ print.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class print{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+        System.out.println("You entered: " + str);
+    }
+}
